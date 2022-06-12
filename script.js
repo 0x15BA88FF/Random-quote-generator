ab1e0544505286fa5cb07c9c195b31d3d50eeb2d
pascal de creator <86390213+pascall-de-creator@users.noreply.github.com>
2022-06-12 10:28:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 var titleEl, authorEl
-quoteEl = document.getElementById('content')
-authorEl = document.getElementById('author')
+var quoteEl = document.getElementById('content')
+var authorEl = document.getElementById('author')
 
-function loadQuote(){
+const loadQuote = () => {
  fetch('https://api.quotable.io/random')
       .then(jsonData => jsonData.json())
       .then(data => response(data))
 
   let response = (data) => {
-    console.log(data)
-    var author, quote;
-    author = data.author
-    quote = data.content
+    var author = data.author
+    var quote = data.content
     
-    quoteEl.innerText = '"'+quote+'"'
-    authorEl.innerText = "-" + author
+    quoteEl.innerText = `"${quote}"`
+    authorEl.innerText = `- ${author}`
 }}
